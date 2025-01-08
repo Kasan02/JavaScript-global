65c6ebd8a0c89eb9c67eaf2ccb36fe624be9dea5
Kasan02 <kasanovd0202@gmail.com>
2025-01-08 19:52:31 +0300

    Не понимаю что не так

--- hw1.js
+++ hw1.js
@@ -1,10 +1,12 @@
-const inputName = document.querySelector('add-form-name');
-const inputComment = document.querySelector('add-form-text');
-const button = document.querySelector('add-form-button');
-
-button.addEventListener('click', function (e) { 
-    const newComment = document.createElement("div");
-    newComment.textContent = `${inputName.value}: ${inputComment.value}`;
-    console.log(inputName);
-    console.log(inputComment);
+const inputName = document.querySelector(".add-form-name");
+const inputComment = document.querySelector(".add-form-text");
+const button = document.querySelector(".add-form-button");
+const list = document.getElementById("list");
+button.addEventListener('click', function () { 
+    const newComment = document.createElement('li');
+    newComment.innerHTML = `${inputName.value}, ${inputComment.value}`;
+    list.appendChild(newComment);
+    console.log(newComment.innerHTML);
+    console.log(list.appendChild(newComment));
+    console.log(newComment.innerHTML = `${inputName.value}, ${inputComment.value}`);
 });

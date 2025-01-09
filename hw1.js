7138dee82fbecb1dd73042c7dac44cfadcd29539
Kasan02 <kasanovd0202@gmail.com>
2025-01-09 21:18:05 +0300

    Добавил script, ничего не поменялось

--- hw1.js
+++ hw1.js
@@ -2,12 +2,15 @@ const inputName = document.getElementById("name");
 const inputComment = document.getElementById("comment");
 const button = document.getElementById("button");
 const list = document.getElementById("list");
+
 button.addEventListener('click', function () { 
     const newComment = document.createElement('li');
     newComment.innerHTML = `${inputName.value}, ${inputComment.value}, ${newDate().toLocaleString()}`;
+
     inputName.value = "";
     inputComment.value = "";
     list.appendChild(newComment);
+
     if (inputName.value === "") {
         alert("Поле ввода не должно быть пустым");
       } else if (inputComment.value === "") {

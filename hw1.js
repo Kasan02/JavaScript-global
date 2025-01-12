915c8b1e07a9e992ac81858e6bf3dc731a8f4434
Kasan02 <kasanovd0202@gmail.com>
2025-01-12 17:28:25 +0300

    Всё добавляется, поле подсвечивается если пустое, проблема позиционирование элементов

--- hw1.js
+++ hw1.js
@@ -1,19 +1,37 @@
 const inputName = document.getElementById("name");
-const inputComment = document.getElementById("comment");
+const inputComment = document.getElementById("text");
 const button = document.getElementById("button");
 const list = document.getElementById("list");
 
-button.addEventListener('click', function () { 
-    const newComment = document.createElement('li');
-    newComment.innerHTML = `${inputName.value}, ${inputComment.value}, ${newDate().toLocaleString()}`;
+button.addEventListener('click', function () {
 
-    inputName.value = "";
-    inputComment.value = "";
-    list.appendChild(newComment);
+  inputName.classList.remove("error");
+  inputComment.classList.remove("error");
 
-    if (inputName.value === "") {
-        alert("Поле ввода не должно быть пустым");
-      } else if (inputComment.value === "") {
-        alert("Поле ввода не должно быть пустым");
-    }
+  if (inputName.value === "") {
+    inputName.classList.add("error");
+    return
+  } else if (inputComment.value === "") {
+    inputComment.classList.add("error");
+    return
+  }
+
+
+
+  const newComment = document.createElement('li');
+  newComment.innerHTML = `${inputName.value}, ${inputComment.value}, ${new Date().toLocaleString()}`;
+  newComment.classList.add("comment");
+  newComment.classList.add("comment-header");
+  newComment.classList.add("comment-body");
+  newComment.classList.add("comment-footer");
+  newComment.classList.add("coments");
+  newComment.classList.add("comment-text");
+  newComment.classList.add("likes");
+  newComment.classList.add("likes-counter");
+  newComment.classList.add("add-form");
+  newComment.classList.add("height");
+
+  inputName.value = "";
+  inputComment.value = "";
+  list.appendChild(newComment);
 });

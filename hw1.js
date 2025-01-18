840ebc871095157fde1c339e813b969976f848d3
Kasan02 <kasanovd0202@gmail.com>
2025-01-18 15:05:11 +0300

    Готово к сдаче

--- hw1.js
+++ hw1.js
@@ -1,44 +1,60 @@
-const button = document.getElementById("button");
-const list = document.getElementById("list");
+const button = document.querySelector('.add-form-button');
+const ulEl = document.querySelector('.comments');
+const nameStyle = document.querySelector('.add-form-name');
+const textStyle = document.querySelector('.add-form-text');
 
 button.addEventListener('click', function () {
 
-  const title = document.querySelector("add-form-name");
-  const text = document.querySelector("add-form-text");
- 
-  title.classList.remove("error");
-  text.classList.remove("error");
+    const name = document.querySelector('.add-form-name').value;
+    const text = document.querySelector('.add-form-text').value;
 
-  if (title.value === "") {
-    title.classList.add("error");
-    return
-  } else if (text.value === "") {
-    text.classList.add("error");
-    return
-  }
+    nameStyle.classList.remove('error');
+    textStyle.classList.remove('error');
 
-  const timeDate = new Date().toLocaleDateString();
+    if (name === '' || name === ' ') {
+        nameStyle.classList.add('error');
+        nameStyle.placeholder = "Вы не ввели имя!"
+        return;
+    } else if (text === '' || text === ' ') {
+        textStyle.classList.add('error');
+        textStyle.placeholder = "Вы не ввели комментарий!"
+        return;
+    } else {
+        nameStyle.style.border = '';
+        nameStyle.style.borderColor = '';
+        document.querySelector('.add-form-name').value = '';
+        nameStyle.placeholder = "Введите ваше имя"
+        textStyle.placeholder = "Введите ваш коментарий"
+        textStyle.style.border = '';
+        textStyle.style.borderColor = '';
+        document.querySelector('.add-form-text').value = '';
+    }
+        let dateTime = new Date();
+        dateTime = dateTime.toLocaleDateString('ru-RU', {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit'
+        }) + ' ' + dateTime.toLocaleTimeString('ru-RU', {
+            hour: '2-digit',
+            minute: '2-digit'
+        });
 
-  const oldHtml = list.innerHTML;
-  console.log(oldHtml);
-  list.innerHTML = oldHtml + `<li class="comment" id="comment">
+        const oldHtml = ulEl.innerHTML;
+        ulEl.innerHTML = oldHtml + `<li class="comment">
                 <div class="comment-header">
-                    <div>${title}</div>
-                    <div>${timeDate}</div>
+                    <div>${name}</div>
+                    <div>${dateTime}</div>
                 </div>
                 <div class="comment-body">
                     <div class="comment-text">
                         ${text}
                     </div>
                 </div>
-                <div class="comment-footer">
+                    <div class="comment-footer">
                     <div class="likes">
                         <span class="likes-counter">0</span>
                         <button class="like-button"></button>
                     </div>
                 </div>
             </li>`;
-
-            name.value = "";
-            text.value = "";
 });
